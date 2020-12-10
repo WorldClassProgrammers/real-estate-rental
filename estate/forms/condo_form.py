40134cc35410820689c51a62a8792b893becfc98
Nanthakarn Limkool <nanthakarn18@gmail.com>
2020-12-11 00:59:33 +0700

    fix user form
    - every number field for user upload is now allow only a positive number

--- estate/forms/condo_form.py
+++ estate/forms/condo_form.py
@@ -1,6 +1,5 @@
 from django.forms import ModelForm, TextInput, Textarea, NumberInput
 from django_google_maps import widgets as map_widgets
-from django_google_maps import fields as map_fields
 from estate.models import Condo
 import json
 
@@ -13,5 +12,10 @@ class CondoForm(ModelForm):
             'name': TextInput(attrs={'class': 'form-control'}),
             'description': Textarea(attrs={'class': "form-control"}),
             'number_of_floors': NumberInput(attrs={'class': 'form-control'}),
-            'address': map_widgets.GoogleMapsAddressWidget(attrs={'data-autocomplete-options': json.dumps({ 'types': ['geocode','establishment'], 'componentRestrictions': {}})})
-            }
+            'address': map_widgets.GoogleMapsAddressWidget(attrs={
+                'data-autocomplete-options': json.dumps({
+                    'types': ['geocode', 'establishment'],
+                    'componentRestrictions': {}
+                })
+            })
+        }
