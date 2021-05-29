3e33551114be11dfba504599dec9eb7c262af77e
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-05-29 15:03:40 +0700

    deploy

--- estateSite/settings.py
+++ estateSite/settings.py
@@ -84,7 +84,6 @@ WSGI_APPLICATION = 'estateSite.wsgi.application'
 
 # Database
 
-
 if config('DATABASE_URL'):
     DATABASES = {
         'default': dj_database_url.config(default=config('DATABASE_URL'))
