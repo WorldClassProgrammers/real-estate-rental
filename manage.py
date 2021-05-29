a8748cdcee5be807683131be55b4d86733a56894
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-05-29 14:18:31 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3.6
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -14,8 +13,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )
-        # from exc
+        ) from exc
+
     execute_from_command_line(sys.argv)
 
 
