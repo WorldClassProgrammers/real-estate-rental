1c28f004fcd6997de451234e2ab26725eb27f393
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-06-21 20:10:17 +0700

    update wsgi.py

--- estateSite/settings.py
+++ estateSite/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 MIDDLEWARE_CLASSES = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'estateSite.urls'
 
