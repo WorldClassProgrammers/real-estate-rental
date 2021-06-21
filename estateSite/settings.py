46a9e8f10c98fb9d82aa5ebe824dd3df6a4ba82e
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-06-21 20:03:24 +0700

    Update settings.py

--- estateSite/settings.py
+++ estateSite/settings.py
@@ -146,9 +146,6 @@ USE_TZ = True
 # ====================================================================
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/estate/static'),
-)
 # ====================================================================
 
 AUTH_USER_MODEL = 'estate.CustomUser'
